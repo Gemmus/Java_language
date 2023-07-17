6fcb9b83ae6011973c7bbc4927ad881c0ee557f9
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-17 11:17:59 +0300

    Create 22_methods.java

---
+++ ExampleSet1/22_methods.java
@@ -0,0 +1,18 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		String birthdayboy = "Joska";
+		int age = 50;
+		
+		bday(birthdayboy, age);
+
+	}
+	
+	static void bday(String name, int age) {
+		
+		System.out.println("Happy Birthday " + name + "! :)");
+		System.out.println("You became " + age + " years old today.");
+		
+	}
+}
