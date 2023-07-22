f76a5a4f319c60f39d3b3f4c2e0ed330099b607d
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-22 09:45:32 +0300

    Create 23_final_keyword.java

---
+++ ExampleSet1/23_final_keyword.java
@@ -0,0 +1,9 @@
+public class Main {
+	
+	public static void main(String[] args) {	
+		
+		final double PI = 3.14159; // final = constant
+		System.out.println("The value of pi is: " + PI);
+		
+	}
+}
