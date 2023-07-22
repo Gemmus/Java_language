69110fe573c21af890ddcd9210798667d1261bad
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-22 10:58:54 +0300

    Create Main.java

---
+++ ExampleSet1/26_Variable_scope/Main.java
@@ -0,0 +1,16 @@
+// Local variables:
+// - declared inside a method
+// - visible only to that method
+
+// Global variables: 
+// - declared outside a method, but within a class
+// - visible to all parts of a class
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		DiceRoller diceRoller = new DiceRoller();
+
+	}
+}
