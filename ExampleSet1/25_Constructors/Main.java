276f50b680e21f98d0b1032f24eee41b67cdc58b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-22 10:48:20 +0300

    Create Main.java

---
+++ ExampleSet1/25_Constructors/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Human human1 = new Human("Robert", 64, 70);
+		Human human2 = new Human("Emma", 36, 52);
+		
+		human1.eat();
+		human2.drink();
+	}
+}
