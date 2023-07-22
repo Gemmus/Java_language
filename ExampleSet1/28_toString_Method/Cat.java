96e0acd0520542fa80260fac9e92cbc801052c71
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-22 11:50:05 +0300

    Add files via upload

---
+++ ExampleSet1/28_toString_Method/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+	
+	String name = "Tapio";
+	String color = "tabby";
+	String favFood = "liver";
+	int age = 9;
+	
+	public String toString() {
+		return name + "\n" + color + "\n" + favFood + "\n" + age;
+	}
+	
+}
