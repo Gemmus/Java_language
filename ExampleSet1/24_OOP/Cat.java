133f81a81f06ca64bf930a84606abbefa48db89e
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-22 10:39:04 +0300

    Add files via upload

---
+++ ExampleSet1/24_OOP/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+	
+	String name = "Tapio";
+	String color = "tabby";
+	String favFood = "liver";
+	int age = 9;
+	
+	void sleep() {
+		System.out.println(name + " is sleeping on the couch.");
+	}
+	
+	void hungry() {
+		System.out.println(name + " is meowing loudly and want to be fed.");
+	}
+	
+}
