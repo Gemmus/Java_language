986fa7496fc1e7771f051f54d46e59cc79566c60
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-23 10:35:27 +0300

    Create Main.java

---
+++ ExampleSet1/35_Abstraction/Main.java
@@ -0,0 +1,12 @@
+// Abstract
+// - abstract classes cannot be instantiated, but they can have a subclass
+// - abstract methods are declared without an implementation
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Car car = new Car();
+		car.go();
+	}
+}
