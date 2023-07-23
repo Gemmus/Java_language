8c6690c5b7f9cc43be606f1d7701c453ff4dad8f
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-23 09:22:41 +0300

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,10 @@
+public class Hotel {
+	
+	void checkin(Pet pet) {
+		System.out.println("The " + pet.name + " is checked in the garage.");
+	}
+	
+	void checkout(Pet pet) {
+		System.out.println("The " + pet.name + " is checked out the garage.");
+	}
+}
