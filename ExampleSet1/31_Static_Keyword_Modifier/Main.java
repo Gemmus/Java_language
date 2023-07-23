ba9b68f01bf404d8185da9b94ff7237aa7630188
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-23 09:33:33 +0300

    Update Main.java

--- ExampleSet1/31_Static_Keyword_Modifier/Main.java
+++ ExampleSet1/31_Static_Keyword_Modifier/Main.java
@@ -1,1 +1,18 @@
+// Static: modifier.
+// A single copy of a variable/method is created and shared. 
+// The class "owns" the static member.
 
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Friends friend1 = new Friends("Andy");
+		Friends friend2 = new Friends("Gary");
+		Friends friend3 = new Friends("Mary");
+		//System.out.println(Friends.numberofFriends);
+		Friends.displayFriends();
+		
+		Friends friend4 = new Friends("Terry");
+		Friends.displayFriends();
+	}
+}
