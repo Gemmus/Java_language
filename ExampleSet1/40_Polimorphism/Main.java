8daa93f401f5811277aac6ec82b47219fee0e42e
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-24 15:52:58 +0300

    Create Main.java

---
+++ ExampleSet1/40_Polimorphism/Main.java
@@ -0,0 +1,18 @@
+// Polymorphism:
+// The ability of an object to identify as more than one type
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		Bicycle bicycle = new Bicycle();
+		Boat boat = new Boat();
+		
+		Vehicle[] racers = {car, bicycle, boat};
+		
+		for(Vehicle x : racers) {
+			x.go();
+		}
+	}
+}
