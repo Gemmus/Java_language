0f83da9e34bd7275101a5e6bb53ed1c5045cc2f3
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-29 08:41:18 +0300

    Create Main.java

---
+++ ExampleSet1/44_FileReader/Main.java
@@ -0,0 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		try {
+			FileWriter newFile = new FileWriter("file1.txt");
+			newFile.write("Tapsa is snoring \nand dreaming.");
+			newFile.append("\n29/07/2023");
+			newFile.close();
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+}
